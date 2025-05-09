6155f6f5e4808e3bd57dd5fafb3a09e88c6ae99a
WeetHet <stas.ale66@gmail.com>
2025-05-09 20:49:56 +0300

    ci: choose boot/switch depending on flake.lock changes

--- packages/by-name/rebuild-server/package.nix
+++ packages/by-name/rebuild-server/package.nix
@@ -16,18 +16,37 @@ writeShellApplication {
     nix-output-monitor
   ];
   text = ''
-    nixos-rebuild switch \
+    if git diff-tree --no-commit-id --name-only -r HEAD | grep -q '^flake\.lock$'; then
+      flake_lock_changed=true
+      command="boot"
+      echo "flake.lock changed in the last commit. Performing '$command' and reboot."
+    else
+      flake_lock_changed=false
+      command="switch"
+      echo "flake.lock did not change. Performing '$command'."
+    fi
+
+    nixos-rebuild "$command" \
       --flake ${self} \
       --use-remote-sudo \
       --use-substitutes \
       --target-host github-actions@${sequoiaAddr} \
       --log-format bar-with-logs
 
-    nixos-rebuild switch \
+    if [ "$flake_lock_changed" = true ]; then
+      echo "Rebooting sequoia..."
+      ssh github-actions@${sequoiaAddr} sudo systemctl reboot
+    fi
+
+    nixos-rebuild "$command" \
       --flake ${self} \
       --use-remote-sudo \
       --use-substitutes \
       --target-host github-actions@${pineAddr} \
       --log-format bar-with-logs
+
+    if [ "$flake_lock_changed" = true ]; then
+      ssh github-actions@${pineAddr} sudo systemctl reboot
+    fi
   '';
 }
