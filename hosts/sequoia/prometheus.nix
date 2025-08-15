df186c97f6c4060691fe997eb0374870dfe85f14
Gleb Smirnov <glebsmirnov0708@gmail.com>
2025-08-15 19:35:42 +0300

    Включил экспортер WireGuard

--- hosts/sequoia/prometheus.nix
+++ hosts/sequoia/prometheus.nix
@@ -1,6 +1,7 @@
 { config, ... }:
 {
   services.prometheus.exporters.wireguard = {
+    enable = true;
     interfaces = [ "wg0" ];
     listenAddress = config.vpn.address;
     openFirewall = true;
