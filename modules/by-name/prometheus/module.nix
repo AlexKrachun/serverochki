8954a73462ccce6c1c1c8d1b03182fbc2a2a5c0b
Gleb Smirnov <glebsmirnov0708@gmail.com>
2025-08-15 13:09:22 +0300

    Исправил сломанный модуль

--- modules/by-name/prometheus/module.nix
+++ modules/by-name/prometheus/module.nix
@@ -13,33 +13,35 @@ in
     };
   };
 
-  sops.secrets.${secretName} = {
-    owner = "root";
-    mode = "0400";
-    restartUnits = [ "wireguard-${wgInterface}" ];
-  };
-  
-  services.prometheus.exporters.node = {
-    enable = true;
-    listenAddress = config.vpn.address;
-    openFirewall = true;
-  };
+  config = {
+    sops.secrets.${secretName} = {
+      owner = "root";
+      mode = "0400";
+      restartUnits = [ "wireguard-${wgInterface}" ];
+    };
 
-  networking.wireguard = {
-    enable = true;
-    interfaces = {
-      ${wgInterface} = {
-        ips = [ "${config.vpn.address}/24" ];
-        privateKeyFile = config.sops.secrets.${secretName}.path;
+    services.prometheus.exporters.node = {
+      enable = true;
+      listenAddress = config.vpn.address;
+      openFirewall = true;
+    };
 
-        peers = [
-          {
-            publicKey = "xjT3bxbeCAobw8zHHInCS2XKunH7erY7XOiJSd3BB2c=";
-            allowedIPs = [ "10.200.200.0/24" ];
-            endpoint = "liferooter.dev:51820";
-            persistentKeepalive = 25;
-          }
-        ];
+    networking.wireguard = {
+      enable = true;
+      interfaces = {
+        ${wgInterface} = {
+          ips = [ "${config.vpn.address}/24" ];
+          privateKeyFile = config.sops.secrets.${secretName}.path;
+
+          peers = [
+            {
+              publicKey = "xjT3bxbeCAobw8zHHInCS2XKunH7erY7XOiJSd3BB2c=";
+              allowedIPs = [ "10.200.200.0/24" ];
+              endpoint = "liferooter.dev:51820";
+              persistentKeepalive = 25;
+            }
+          ];
+        };
       };
     };
   };
